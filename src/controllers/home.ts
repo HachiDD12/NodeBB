b17f39c06ad843f959851d6dbc562eb162cdfa8a
HachiDD12 <chenzhuofan123@gmail.com>
2023-01-26 00:25:22 -0500

    TODO: typecheck res in pluginHook

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,70 +1,78 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pluginHook = exports.rewrite = void 0;
-const url_1 = __importDefault(require("url"));
+import url from 'url';
+import * as express from 'express';
+import { SettingsObject } from '../types';
 // import { Url } from 'url';
-const plugins_1 = __importDefault(require("../plugins"));
-const meta_1 = __importDefault(require("../meta"));
-const user_1 = __importDefault(require("../user"));
-function adminHomePageRoute() {
+import plugins from '../plugins';
+import meta from '../meta';
+import user from '../user';
+
+type urlparse = url.UrlWithParsedQuery;
+type Request = express.Request;
+type Response = express.Response;
+type Next = express.NextFunction;
+
+function adminHomePageRoute() : string {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return ((meta_1.default.config.homePageRoute === 'custom' ? meta_1.default.config.homePageCustom : meta_1.default.config.homePageRoute) || 'categories').replace(/^\//, '');
+    return <string>((meta.config.homePageRoute === 'custom' ? meta.config.homePageCustom : meta.config.homePageRoute) || 'categories').replace(/^\//, '');
 }
-async function getUserHomeRoute(uid) {
+
+
+async function getUserHomeRoute(uid : number) : Promise<string> {
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const settings = (await user_1.default.getSettings(uid));
+    const settings : SettingsObject = <SettingsObject>(await user.getSettings(uid));
     let route = adminHomePageRoute();
+
     if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
         route = (settings.homePageRoute || route).replace(/^\/+/, '');
     }
+
     return route;
 }
-async function rewrite(req, res, next) {
+
+export async function rewrite(req : Request & SettingsObject, res : Response, next : Next) {
     if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
         return next();
     }
     let route = adminHomePageRoute();
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    if (meta_1.default.config.allowUserHomePage) {
+    if (<boolean>meta.config.allowUserHomePage) {
         route = await getUserHomeRoute(req.uid);
     }
-    let parsedUrl;
+
+    let parsedUrl : urlparse;
     try {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        parsedUrl = url_1.default.parse(route, true);
-    }
-    catch (err) {
+        parsedUrl = url.parse(route, true);
+    } catch (err) {
         return next(err);
     }
+
     const { pathname } = parsedUrl;
     const hook = `action:homepage.get:${pathname}`;
     // The next line calls a function in a module that has not been updated to TS yet
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    if (!plugins_1.default.hooks.hasListeners(hook)) {
+    if (!plugins.hooks.hasListeners(hook)) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
-    }
-    else {
+    } else {
         res.locals.homePageRoute = pathname;
     }
     req.query = Object.assign(parsedUrl.query, req.query);
+
     next();
 }
-exports.rewrite = rewrite;
-async function pluginHook(req, res, next) {
+
+export async function pluginHook(req: Request, res, next: Next):Promise<void> {
     const hook = `action:homepage.get:${res.locals.homePageRoute}`;
-    await plugins_1.default.hooks.fire(hook, {
+
+    await plugins.hooks.fire(hook, {
         req: req,
         res: res,
         next: next,
     });
 }
-exports.pluginHook = pluginHook;
